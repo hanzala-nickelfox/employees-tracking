8857ff19f2cf64ef31d8befb8607ddc8babeef16
Neeraj Yadav <neeraj.k@nickelfox.co>
2022-03-14 17:29:51 +0530

    (feature/neeraj-signUp):

--- src/pages/public/login/index.js
+++ src/pages/public/login/index.js
@@ -1,74 +1,78 @@
-import React from "react";
-import { Typography, TextField, Button, Grid, Divider } from "@mui/material";
-import { Formik, Field } from "formik";
-import AppDispatcher from "redux/dispatchers/appDispatcher";
-import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
+// import "./signUp.scss";
+import BasicBtn from "../../../components/shared/Basic-btn.js";
+import TextField from "../../../components/shared/FormField.js";
+import { Alert } from "react-bootstrap";
 
 const Login = () => {
-  const history = useHistory();
+  const [emaillog, setEmaillog] = useState("");
+  const [passwordlog, setPasswordlog] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [home, sethome] = useState(true);
 
-  const userLogin = () => {
-    AppDispatcher.setUserLoggedIn({
-      token: "djkhfkdhfdhfs",
-      user: { name: "Test", email: "test@gmail.com" }
-    });
-    history.push("/u/dashboard");
-  };
+  function handlelogin(e) {
+    e.preventDefault();
+    let mail = localStorage.getItem("Email").replace(/"/g, "");
+    let pass = localStorage.getItem("password").replace(/"/g, "");
+
+    if (!emaillog || !passwordlog) {
+      setFlag(true);
+    } else if (passwordlog === pass && emaillog === mail) {
+      setFlag(true);
+    } else {
+      sethome(!home);
+      setFlag(false);
+    }
+  }
 
   return (
-    <React.Fragment>
-      <Typography variant="h3">Welcome Back!</Typography>
-      <Typography variant="subtitle">
-        Enter your email and password to login
-      </Typography>
-      <Grid container spacing={2}>
-        <Divider />
-        <Formik
-          initialValues={{
-            email: "",
-            password: ""
-          }}
-          onSubmit={userLogin}>
-          {({ isValid, handleSubmit }) => (
-            <React.Fragment>
-              <Grid item xs={12}>
-                <Field
-                  id="email"
-                  name="email"
-                  placeholder="Enter your email"
-                  as={TextField}
-                  variant="outlined"
-                  label="Email"
-                />
-              </Grid>
+    <>
+      {home ? <h1>home</h1> : <h1>hello</h1>}
+      <section className="signUp">
+        <div className="signUp__container">
+          <h3 className="signUp__container__title">Login</h3>
+          <div className="signUp__container__form">
+            <form onSubmit={handlelogin}>
+              <br />
+              <TextField
+                type="email"
+                name={emaillog}
+                placeholder="Email"
+                onChange={(e) => setEmaillog(e.target.value)}
+              />
+              <br /> <br />
+              <TextField
+                type="number"
+                name={passwordlog}
+                placeholder="Password"
+                onChange={(e) => setPasswordlog(e.target.value)}
+              />
+              <br /> <br />
+              <BasicBtn
+                text="submit"
+                // name={submit}
+                onChange={handlelogin()}></BasicBtn>
+              <h3 className="d-iniline">
+                if you dont have account sign up.
+                <BasicBtn
+                  text="Sign up"
+                  name="Sign up"
+                  className="w-25"></BasicBtn>
+              </h3>
+            </form>
+          </div>
+        </div>
 
-              <Grid item xs={12}>
-                <Field
-                  id="password"
-                  name="password"
-                  type="password"
-                  placeholder="Enter your password"
-                  as={TextField}
-                  variant="outlined"
-                  label="Password"
-                />
-              </Grid>
-
-              <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  disabled={!isValid}
-                  variant="outlined"
-                  size="large"
-                  onClick={handleSubmit}>
-                  Submit
-                </Button>
-              </Grid>
-            </React.Fragment>
+        {flag &&
+          (<Alert color="primary" variant="danger">
+            <h3>Please fill the details.</h3>
+          </Alert>)(
+            <Alert color="primary" variant="success">
+              false
+            </Alert>
           )}
-        </Formik>
-      </Grid>
-    </React.Fragment>
+      </section>
+    </>
   );
 };
 
--- src/pages/public/signup/index.js
+++ src/pages/public/signup/index.js
@@ -1,39 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import "./signUp.scss";
 import BasicBtn from "../../../components/shared/Basic-btn.js";
 import TextField from "../../../components/shared/FormField.js";
+import { Alert } from "react-bootstrap";
+import Login from "../login/index";
 
 const SignUp = () => {
+  const [namelog, setNamelog] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [submit, setSubmit] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [login, setLogin] = useState(true);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.warn(submit, "check=====");
+
+    alert("working");
+
+    if (!namelog || !email || !password || !confirmPassword) {
+      setFlag(true);
+    } else {
+      setFlag(false);
+      localStorage.setItem("Email", JSON.stringify(email));
+      localStorage.setItem("Password", JSON.stringify(password));
+      localStorage.setItem("Namelog", JSON.stringify(namelog));
+      console.log(localStorage, "Email================");
+      setLogin(!login);
+    }
+  }
+
+  function hanldeClick() {
+    alert("hello");
+    // setLogin(true);
+  }
+
+  function AlerButton() {
+    // alert("working");
+    console.log(login, "check=====");
+  }
+
   return (
     <>
-      <section className="signUp">
-        <div className="signUp__container">
-          <h3 className="signUp__container__title">Sign Up</h3>
-          <div className="signUp__container__form">
-            <form>
-              <br />
-              <TextField type="text" name="Full name" placeholder="Full name" />
-              <br /> <br />
-              <TextField type="email" name="Email" placeholder="Email" />
-              <br /> <br />
-              <TextField type="number" name="Password" placeholder="Password" />
-              <br /> <br />
-              <TextField
-                type="number"
-                name="Confirm password"
-                placeholder="Confirm password"
-              />
-              <br /> <br />
-              <BasicBtn text="Submit"></BasicBtn>
-              <button className="btn btn-success">button</button>
-            </form>
+      <button onClick={AlerButton()}>Alert button</button>
+      {login !== false ? (
+        <section className="signUp">
+          <div className="signUp__container">
+            <h3 className="signUp__container__title">Sign Up</h3>
+            <div className="signUp__container__form">
+              <form onSubmit={handleSubmit}>
+                <br />
+                <TextField
+                  type="text"
+                  name={namelog}
+                  placeholder="Full name"
+                  onChange={(e) => setNamelog(e.target.value)}
+                />
+                <br /> <br />
+                <TextField
+                  type="email"
+                  name={email}
+                  placeholder="Email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <br /> <br />
+                <TextField
+                  type="number"
+                  name={password}
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <br /> <br />
+                <TextField
+                  type="number"
+                  name={confirmPassword}
+                  placeholder="Confirm password"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <br /> <br />
+                <BasicBtn
+                  text="submit"
+                  name={submit}
+                  onChange={(e) => setSubmit(e.target.value)}></BasicBtn>
+                <h3 className="d-iniline">
+                  Already sign in,{" "}
+                  <BasicBtn
+                    text="login"
+                    name={login}
+                    className="w-25"
+                    onClick={() => hanldeClick()}></BasicBtn>
+                </h3>
+              </form>
+            </div>
           </div>
-        </div>
 
-        <div className="signUp__container">
-          <h3>Resultes</h3>
-        </div>
-      </section>
+          {flag &&
+            (<Alert color="primary" variant="danger">
+              <h3>Please fill correct info.</h3>
+            </Alert>)(
+              <Alert color="primary" variant="success">
+                false
+              </Alert>
+            )}
+        </section>
+      ) : (
+        <Login />
+      )}
     </>
   );
 };
