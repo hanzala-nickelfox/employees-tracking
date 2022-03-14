58b7f7173f0d62eaa78841bdca34bb0394cc790d
MehranSiddiqui <mehran.siddiqui@nickelfox.com>
2022-03-14 10:57:45 +0530

    'slice updated to signup'

--- src/redux/slices/signupSlice.js
+++ src/redux/slices/signupSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userData: {}
+  userData: {},
+  storage: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : []
 };
 const signUpSlice = createSlice({
   name: "signup",
-  storage: localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
-    : [],
-
   initialState,
+
   reducers: {
     signUp: (state, action) => {
-      state.value = action.payload;
-      console.log(action.payload);
+      state.userData = action.payload;
 
       if (action.payload.email !== "") {
         if (action.payload.password !== action.payload.confirmPassword) {
           alert("password and confirm password do not match");
         } else {
-          state.storage.push(state.value);
-          localStorage.setItem("user", JSON.stringify(state.user));
+          state.storage.push(state.userData);
+          localStorage.setItem("user", JSON.stringify(state.storage));
         }
       }
     }
