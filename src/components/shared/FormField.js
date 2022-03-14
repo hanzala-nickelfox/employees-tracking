eca9ac16f4671ad5b2a66a8e3bbeb1dee3e25466
Neeraj Yadav <neeraj.k@nickelfox.co>
2022-03-14 12:23:18 +0530

    (feature/neeraj-signUp): form layout

--- src/components/shared/FormField.js
+++ src/components/shared/FormField.js
@@ -1,19 +1,20 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { TextField } from "@mui/material";
+import React from "react";
 
 function FormField(props) {
-
-    return (
-        <>
-            <TextField className='w-100'
-                id={props.id}
-                label={props.label}
-                variant={props.variant}
-                type={props.type}
-                name={props.name}
-            />
-        </>
-    )
+  return (
+    <>
+      <TextField
+        className="w-100"
+        id={props.id}
+        label={props.label}
+        variant={props.variant}
+        type={props.type}
+        name={props.name}
+        placeholder={props.placeholder}
+      />
+    </>
+  );
 }
 
-export default FormField
+export default FormField;
--- src/pages/public/signup/index.js
+++ src/pages/public/signup/index.js
@@ -1,7 +1,36 @@
 import React from "react";
+import "./signUp.scss";
+import BasicBtn from "../../../components/shared/Basic-btn.js";
+import TextField from "../../../components/shared/FormField.js";
 
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="signUp">
+        <div className="signUp__container">
+          <div className="signUp__container__title">Sign Up</div>
+          <div className="signUp__container__form">
+            <form>
+              <br />
+              <TextField type="text" name="Full name" placeholder="Full name" />
+              <br /> <br />
+              <TextField type="email" name="Email" placeholder="Email" />
+              <br /> <br />
+              <TextField type="number" name="Password" placeholder="Password" />
+              <br /> <br />
+              <TextField
+                type="number"
+                name="Confirm password"
+                placeholder="Confirm password"
+              />
+              <br /> <br />
+              <BasicBtn text="Submit"></BasicBtn>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SignUp;
