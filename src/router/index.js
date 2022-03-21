2af215a7ca55cfae620cd6fff3c246f1f4b611b2
Hiten Soni <hiten@nickelfox.co>
2022-03-21 11:26:37 +0530

    feat(dashboard): Ui complete

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Error404 from "pages/Error404";
 import SignUp from "pages/public/signup";
+import Dashboard from "pages/private/Dashboard";
+
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<SignUp />} />
-
+        <Route path="/dashboard" element={<Dashboard />} />
         {/* 404 page route */}
         <Route path="*" element={<Error404 />} />
       </Routes>
